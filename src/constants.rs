a746f5796b465618a87740befc25858f8b624058
Gustash <gustavotcparreira@gmail.com>
2023-08-18 19:30:25 +0100

    fix: max chunk size documentation

--- src/constants.rs
+++ src/constants.rs
@@ -3,5 +3,5 @@ use lazy_static::lazy_static;
 lazy_static! {
     pub(crate) static ref BASE_URL: &'static str = "https://www.indiegala.com";
     pub(crate) static ref CONTENT_URL: &'static str = "https://content.indiegalacdn.com";
-    pub(crate) static ref MAX_CHUNK_SIZE: u64 = 1048576; // 8 MiB
+    pub(crate) static ref MAX_CHUNK_SIZE: u64 = 1048576; // 1 MiB
 }
